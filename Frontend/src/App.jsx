35f2ca5847d76cd7f8e94f6cc5186d37a61b19d2
Amanjarngal <you@example.com>
2025-10-07 01:35:09 +0530

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+
+function App() {
+  return (
+   <Router>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='*' element={<div>404 Not Found</div>}/>
+    </Routes>
+   </Router>
+  )
+}
+
+export default App
