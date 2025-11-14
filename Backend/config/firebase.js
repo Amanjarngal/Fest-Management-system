e3a0092c2328dc3c68256f23575fd8e59a9beaab
Amanjarngal <you@example.com>
2025-11-15 02:01:10 +0530

    json update

--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -1,21 +1,13 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-// __dirname workaround for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Load service account JSON from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
 
-// Load service account key
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, process.env.FIREBASE_SERVICE_ACCOUNT), "utf8")
-);
-
+// Initialize Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const auth = admin.auth();
 
-export { admin, auth }; // 👈 Export both
+export { admin, auth };
